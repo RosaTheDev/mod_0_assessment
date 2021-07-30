56daf9e847d7387ee9e7fd05facedc6884e1a29b
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-07-30 11:06:33 -0400

    Create a Laptop object

---
+++ models/modelA.js
@@ -0,0 +1,13 @@
+class Laptop {
+  constructor() 
+  {
+    this.brand = "Apple";
+    this.processor = "M1";
+    this.year = 2020;
+    this.size = 13;
+    this.fast = true;
+    this.touchbar = true;
+    this.accessories = ["Usb C doc", "External Monitor", "Airpods"];
+    this.features = ["Touchbar", "No butterfly keys", "New Processor"];
+  }
+}
