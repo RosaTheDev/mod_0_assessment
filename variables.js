c345e2c2dd6fd71687fc40fb33920f3cf5dac800
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-07-30 10:58:40 -0400

    Complete program specific challege

---
+++ variables.js
@@ -0,0 +1,12 @@
+var animal = "Honey Badger";
+var isCuddly = false;
+animal = "Mongoose";
+console.log(animal);
+var num8 = 8;
+var num3 = 3;
+var quotient = num3 / num8;
+console.log(quotient);
+var favoriteFilms = ['Judas and The Black Messiah', 'Get Out', 'Malcolm X'];
+favoriteFilms.shift();
+console.log(favoriteFilms);
+console.log(animal.length);
