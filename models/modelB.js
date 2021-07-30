00d69bd11637650ae33876ee210753a6a5a8945a
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-07-30 11:13:38 -0400

    Create House Object

---
+++ models/modelB.js
@@ -0,0 +1,13 @@
+class House {
+  constructor()
+  {
+    this.located = "Fayetteville, NC";
+    this.built = "1998";
+    this.bathrooms = 3;
+    this.bedrooms = 4;
+    this.neighborhoodSafe = true;
+    this.goodSchools = false;
+    this.houseProblems = ["Haunted", "Dead body", "Termites"];
+    this.schools = ["South View High", "South View Middle", "Jack Britt High"];
+  }
+}
